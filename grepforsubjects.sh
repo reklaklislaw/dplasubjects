d36bef149e1d9156e74fd34764a252b2f4eb9496
reklaklislaw <thomkerr@gmail.com>
2014-03-20 23:47:48 -0400

    improve grep script

--- grepforsubjects.sh
+++ grepforsubjects.sh
@@ -5,21 +5,48 @@
 infile=$1
 outfile=$2
 
-#lists
-r1="(\"subject\":\[.+?(?=\]\},)[\]]|"
+echo "getting total subject field count..."
+total=$(LC_ALL=C grep -o -P "\"subject\":" $infile | wc -l)
+echo "total number of subject fields: $total" 
 
-#strings
-r2="\"subject\":\"[^\"]*\"[^,^\}]*)"
+echo "getting empty subject field count..."
+empty=$(LC_ALL=C grep -o -P "\"subject\":(\[\]|null|\[null\])" $infile | wc -l)
+echo "total empty: $empty"
 
-regex=$r1$r2
+echo "getting subject lists..."
+lists=$(LC_ALL=C grep -o -P \
+    "\"subject\":(\[\".+?\"(?=(\]|,))\]|\[\{.+?\}(?=(\]|,|\}))\]|\{.+?\}(?=(\}|,|))\})" $infile)
+if [ -z "$lists"  ]
+then
+    list_count=0
+else
+    list_count=$(echo "{$lists}" | wc -l)
+fi
+echo "total lists: $list_count"
 
-LC_ALL=C zegrep -o "$regex" $infile > $outfile
+echo "getting subject strings..."
+strings=$(LC_ALL=C grep -o -P "\"subject\":\"[^\"]*\"[^,^\}]*" $infile)
+if [ -z "$strings"  ]
+then
+    string_count=0
+else
+    string_count=$(echo "{$strings}" | wc -l)
+fi
 
-sed -i 's/"subject":/{"subject":/g' $outfile
-sed -i 's/.$/&},/g' $outfile
-sed -i '1i[' $outfile
-sed -i '$s/.$/]/g' $outfile
+echo "total strings: $string_count"
 
+matched=$(($list_count+$string_count+$empty))
 
-
-
+if [ $matched == $total ]
+then 
+    echo "matched all subjects"
+    echo "{$lists}" > $outfile
+    echo "{$strings}" >> $outfile
+    #echo "formatting..."
+    #sed -i 's/"subject":/{"subject":/g' $outfile
+    #sed -i 's/.$/&},/g' $outfile
+    #sed -i '1i[' $outfile
+    #sed -i '$s/.$/]/g' $outfile
+else
+    echo "FAILED TO MATCH ALL SUBJECTS... matched: $matched   total: $total" 
+fi
