c020006b15bed2fca73c94a228488c58393b0067
reklaklislaw <thomkerr@gmail.com>
2014-03-21 23:33:03 -0400

    improvements to grep script

--- grepforsubjects.sh
+++ grepforsubjects.sh
@@ -10,12 +10,19 @@ total=$(LC_ALL=C grep -o -P "\"subject\":" $infile | wc -l)
 echo "total number of subject fields: $total" 
 
 echo "getting empty subject field count..."
-empty=$(LC_ALL=C grep -o -P "\"subject\":(\[\]|null|\[null\])" $infile | wc -l)
+empty=$(LC_ALL=C grep -o -P "\"subject\":(\[\]|null|\[null,\]*)" $infile | wc -l)
 echo "total empty: $empty"
 
 echo "getting subject lists..."
-lists=$(LC_ALL=C grep -o -P \
-    "\"subject\":(\[\".+?\"(?=(\]|,))\]|\[\{.+?\}(?=(\]|,|\}))\]|\{.+?\}(?=(\}|,|))\})" $infile)
+
+l0="\"subject\":("
+l1="\[\".+?\"(?=(\]|,))\]|"
+l2="\[\{.+?(?=(\}\]))\}\]|"
+l3="\[\{.+?\}(?=([\}]{3}))\})"
+
+listregex=$l0$l1$l2$l3
+
+lists=$(LC_ALL=C grep -o -P $listregex $infile)
 if [ -z "$lists"  ]
 then
     list_count=0
@@ -23,6 +30,25 @@ else
     list_count=$(echo "{$lists}" | wc -l)
 fi
 echo "total lists: $list_count"
+
+
+echo "getting subject dicts..."
+
+d0="\"subject\":("
+d1="(\{.+?((?=(\]\}))(\]\}\}|\]\})|(?=\}\})\}))|"
+d2="\{\".+?(?=\},)\})"
+
+dictregex=$d0$d1$d2
+
+dicts=$(LC_ALL=C grep -o -P $dictregex $infile)
+if [ -z "$dicts"  ]
+then
+    dict_count=0
+else
+    dict_count=$(echo "{$dicts}" | wc -l)
+fi
+echo "total dicts: $dict_count"
+
 
 echo "getting subject strings..."
 strings=$(LC_ALL=C grep -o -P "\"subject\":\"[^\"]*\"[^,^\}]*" $infile)
@@ -35,18 +61,31 @@ fi
 
 echo "total strings: $string_count"
 
-matched=$(($list_count+$string_count+$empty))
+
+matched=$(($list_count+$dict_count+$string_count+$empty))
 
 if [ $matched == $total ]
 then 
     echo "matched all subjects"
-    echo "{$lists}" > $outfile
-    echo "{$strings}" >> $outfile
-    #echo "formatting..."
-    #sed -i 's/"subject":/{"subject":/g' $outfile
-    #sed -i 's/.$/&},/g' $outfile
-    #sed -i '1i[' $outfile
-    #sed -i '$s/.$/]/g' $outfile
+    if [ -n "$lists" ]
+    then
+	echo "$lists" > $outfile
+    fi
+    if [ -n "$dicts" ]
+    then
+	echo "$dicts" > $outfile
+    fi
+    if [ -n "$strings" ]
+    then
+	echo "$strings" >> $outfile
+    fi
+    echo "formatting..."
+    sed -i 's/"subject":/{"subject":/g' $outfile
+    sed -i 's/.$/&},/g' $outfile
+    sed -i '1i[' $outfile
+    sed -i '$s/.$/]/g' $outfile
+    echo 'verifying json'
+    jsonlint $outfile
 else
     echo "FAILED TO MATCH ALL SUBJECTS... matched: $matched   total: $total" 
 fi
