8e1eb9e0d300351997b7b87968c460cb66d77e1a
reklaklislaw <thomkerr@gmail.com>
2014-04-02 21:57:24 -0400

    add json2pickledset to remove duplicate entries and pickle for further processing

---
+++ json2pickledset.py
@@ -0,0 +1,50 @@
+import json, pickle
+import sys
+
+
+def unroll(data):
+    subjects = set()
+    if isinstance(data, dict):
+        values = data.values()
+    elif isinstance(data, list):
+        values = data
+    for d in values:
+        if d is None:
+            continue
+        elif isinstance(d, str):
+            subjects.add(d)
+        elif isinstance(d, (list, dict)):
+            for item in unroll(d):
+                subjects.add(item)
+        else:
+            raise TypeError('encountered a list of ' + str(type(d)))
+    return subjects
+
+
+def main(in_file, out_file):
+
+    with open(in_file, 'r') as f:
+        j = json.load(f)
+
+    subjects = set()
+    for dct in j:
+        s = dct['subject']
+        if isinstance(s, (list, dict)):
+            for item in unroll(s):
+                subjects.add(item)
+        elif isinstance(s, str):
+            subjects.add(s)
+        else:
+            raise TypeError('encountered a ' + str(type(s)))
+
+    with open(out_file, 'wb') as f:
+        pickle.dump(subjects, f) 
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        in_file = sys.argv[1]
+        out_file = sys.argv[2]
+        main(in_file, out_file)
+    else:
+        raise ValueError('invalid arguments')
