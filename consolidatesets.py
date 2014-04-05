46df86581ed12c615cee248729252ca4ff4d1417
reklaklislaw <thomkerr@gmail.com>
2014-04-04 23:29:44 -0400

    add substring splitting

--- consolidatesets.py
+++ consolidatesets.py
@@ -10,10 +10,37 @@ def main(glob_pattern, out_file):
         with open(pset_f, 'rb') as f:
             sset = pickle.load(f)
         for s in sset:
-            subjects.add(s)
+            if s:
+                subjects.add(s)
+                for i in get_substrings(s):
+                    subjects.add(i)
     with open(out_file, 'wb') as f:
         pickle.dump(subjects, f)
+         
         
+def get_substrings(string):
+    sub = set()
+    patterns = (';', ',', '--', ' ')
+    for p in patterns:
+        s = string.split(p)
+        for i in s:
+            item = i
+            for r in patterns:
+                item = item.rstrip(r).lstrip(r)
+            if item:
+                sub.add(item)
+                for _p in patterns:
+                    _s = item.split(_p)
+                    for _i in _s:
+                        _item = _i
+                        for r in patterns:
+                            _item = _item.rstrip(r).lstrip(r)
+                        if _item:
+                            sub.add(_item)
+    if len(s) > 1:
+        return sub
+    else:
+        return []
 
 
 if __name__ == '__main__':
