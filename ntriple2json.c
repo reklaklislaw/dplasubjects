5595813d203d6fd9457e0ce9dcd29f9fa78b2b6c
reklaklislaw <thomkerr@gmail.com>
2014-04-13 21:43:27 -0400

    improve hashing; various changes

--- ntriple2json.c
+++ ntriple2json.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc!=3) 
+  if (argc!=4) 
     {
       printf("invalid arguments\n");
       exit(1);
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
   char in[100], out[100];
   strcpy(in, argv[1]);
   strcpy(out, argv[2]);
+
+  int bucket_size = atoi(argv[3]);
 
   FILE *in_file = fopen(in, "r");
   if (in_file==NULL) 
@@ -31,7 +33,7 @@ int main(int argc, char *argv[])
       exit(1);
     }
   
-  parse(in_file, out_file);
+  parse(in_file, out_file, bucket_size);
       
   fclose(in_file);
   fclose(out_file);
@@ -39,16 +41,16 @@ int main(int argc, char *argv[])
 }
 
 
-void parse(FILE *in_file, FILE *out_file)
+void parse(FILE *in_file, FILE *out_file, int bucket_size)
 { 
   struct ntriple nt;
   int so_count = 0;
-  int so_size = 6000000;
+  int so_size = bucket_size;
   struct subjectObject *subjObj = malloc(so_size * sizeof(struct subjectObject));
-  int hash_size = 1000;
-  int *hashes = malloc(hash_size * sizeof(int));
+  int hash_size = 10000;
+  size_t *hashes = malloc(hash_size * sizeof(size_t));
   int hash_count = 0;
-  int collisions = 0;
+  size_t *collisions = malloc(so_size * sizeof(size_t));
   int eof = 0;
   while (1) 
     {
@@ -62,47 +64,55 @@ void parse(FILE *in_file, FILE *out_file)
 	  so_size += 1000;
 	}
 
+
+      if (hash_count == hash_size)
+	{
+	  hashes = realloc(hashes, (hash_size+10000) * sizeof(size_t));
+	  if (hashes==NULL)
+	    {
+	      printf("failed to realloc hashes\n");
+	      exit(1);
+	    }
+	  hash_size += 10000;
+	}
+	  
       nt = get_next_ntriple(in_file, &eof);
       char *id = get_id(nt.subject);
       if (id=='\0')
 	continue;
 
-      int hash = get_hash(id);
-      //printf("%d %s\n", hash, id);
-      if (subjObj[hash].id != '\0' && strcmp(subjObj[hash].id, id) == 0) 
+      size_t hash = get_hash(id, bucket_size);
+      //printf("%lu %s\n", hash, id);
+      
+      if (subjObj[hash].id != '\0') 
 	{ 
-	  //printf("adding to hash %d:  id:%s url:%s\n", hash, id, subjObj[hash].url);
-	  add_ntriple_data(&subjObj[hash], nt);
-	}
-      else if (subjObj[hash].id == '\0')
-	{	  
-	  
-	  if (hash_count == hash_size)
+	  if (strcmp(subjObj[hash].id, id) == 0) 
 	    {
-	      hashes = realloc(hashes, (hash_size+1000) * sizeof(int));
-	      if (hashes==NULL)
-		{
-		  printf("failed to realloc hashes\n");
-		  exit(1);
-		}
-	      hash_size += 1000;
+	      //printf("adding to hash %lu:  id:%s url:%s\n", hash, id, subjObj[hash].url);
+	      add_ntriple_data(&subjObj[hash], nt);
 	    }
-	  
+	  else {
+	    //TODO something about collisions
+	    //init_new_entry(&subjObj[p], nt, id);
+	    //add_ntriple_data(&subjObj[p], nt);
+	    //hashes[hash_count] = p;
+	    //hash_count += 1;
+	    collisions[hash] += 1;
+	    //so_count++;
+	  }
+	}
+
+      else
+	{	  
+	  //printf("creating new at hash %d: %s\n", hash, id);
+	  init_new_entry(&subjObj[hash], nt, id);
+	  add_ntriple_data(&subjObj[hash], nt);
 	  hashes[hash_count] = hash;
 	  hash_count += 1;
-
-	  //printf("creating new at hash %d: %s\n", hash, id);
-	  alloc_new_entry(&subjObj[hash]);
-	  subjObj[hash].id = id;
-	  subjObj[hash].url = malloc(nt.subjectSize * sizeof(char) + 2);
-	  int i, j;
-	  for (i=1, j=0; i<nt.subjectSize-1; i++, j++)
-	    subjObj[hash].url[j] = nt.subject[i];
-	  subjObj[hash].url[j] = '\0';
-	  //printf("--id:%s  url:%s\n", id, subjObj[hash].url);
-	  add_ntriple_data(&subjObj[hash], nt);
+	  collisions[hash] = 0;
 	  so_count++;
 	}
+
 
       free(nt.subject);
       free(nt.predicate);
@@ -111,37 +121,40 @@ void parse(FILE *in_file, FILE *out_file)
       if (eof)
 	break;
     }
-  
-  int i, h;
+
+  int i;
+  size_t h;
+  float hc = 0.0;
+  float cc = 0.0;
   for (i=0; i<hash_count; i++)
     {
       h = hashes[i];
       if (subjObj[h].id == '\0')
 	continue;
-      else
-	printf("id: %s  url: %s\n", subjObj[h].id, subjObj[h].url);
+      else {
+	hc += 1.0;
+	cc += collisions[h];
+	printf("id: %s  url: %s  hash:%lu  collisions:%lu \n ", 
+	       subjObj[h].id, subjObj[h].url, h, collisions[h]);
+      }
     }
   
+  printf("average collisions per hash:%f\n", cc/hc);
 
 }
 
-
-int get_hash(char *id)
+size_t get_hash(char *id, int bucket_size)
 {
-  int i = 0;
-  int j = 2;
-  char *buf = malloc(20 * sizeof(char));
-  while (1)
-    {      
-      buf[i] = id[j];
-      if (id[j] == '\0')
-	break;
-      i++;
-      j++;
+  const bsize = 32;
+  size_t b = rand() % 5000000;
+  while (*id)
+    {
+      b ^= *id++;
+      b = ((b << 2) | (b >> (bsize - 2)));
     }
-  
-  int hash = atoi(buf) % 6000000;
-  return hash;
+
+  //printf("hash:%lu\n", b % 300007);
+  return b % bucket_size;
 }
 
 
@@ -153,12 +166,11 @@ void add_ntriple_data(struct subjectObject *subjObj, struct ntriple nt)
       subjObj->prefLabel = malloc(nt.objectSize * sizeof(char) + 1);
       strcpy(subjObj->prefLabel, nt.object);
     }
-  
+
   else if (match_tag(nt.predicate, "altLabel")) 
     {
       if (subjObj->altCount == subjObj->altSize) 
 	{
-	  printf("realloc\n");
 	  *subjObj->altLabel = realloc(*subjObj->altLabel, 
 				       (subjObj->altSize + 10) * sizeof(char));
 	  if (*subjObj->altLabel==NULL) 
@@ -254,7 +266,7 @@ void add_ntriple_data(struct subjectObject *subjObj, struct ntriple nt)
       subjObj->related[subjObj->relatedCount][j] = '\0';
       subjObj->relatedCount += 1;
     }
-  
+
 }
 
 int match_tag(char *predicate, char *tag)
@@ -285,23 +297,37 @@ int match_tag(char *predicate, char *tag)
 }
 
 
+void init_new_entry(struct subjectObject *subjObj, struct ntriple nt, char *id)
+{
+  alloc_new_entry(subjObj);
+  strcpy(subjObj->id, id);
+  subjObj->url = malloc(nt.subjectSize * sizeof(char) + 2);
+  
+  int i, j;
+  for (i=1, j=0; i<nt.subjectSize-1; i++, j++)
+    subjObj->url[j] = nt.subject[i];
+  subjObj->url[j] = '\0';
+}
+
 void alloc_new_entry(struct subjectObject *subjObj)
 {
-  subjObj->altLabel = malloc(10 * sizeof(char));
+  int size = 25;
+  subjObj->id = malloc(size * sizeof(char));
+  subjObj->altLabel = malloc(size * sizeof(char));
+  subjObj->narrower = malloc(size * sizeof(char));
+  subjObj->broader = malloc(size * sizeof(char));
+  subjObj->closeMatch = malloc(size * sizeof(char));
+  subjObj->related = malloc(size * sizeof(char));
   subjObj->altCount = 0;
-  subjObj->altSize = 10;
-  subjObj->narrower = malloc(10 * sizeof(char));
+  subjObj->altSize = size;
   subjObj->narrowerCount = 0;
-  subjObj->narrowerSize = 10;
-  subjObj->broader = malloc(10 * sizeof(char));
+  subjObj->narrowerSize = size;
   subjObj->broaderCount = 0;
-  subjObj->broaderSize = 10;
-  subjObj->closeMatch = malloc(10 * sizeof(char));
+  subjObj->broaderSize = size;
   subjObj->closeMatchCount = 0;
-  subjObj->closeMatchSize = 10;
-  subjObj->related = malloc(10 * sizeof(char));
+  subjObj->closeMatchSize = size;
   subjObj->relatedCount = 0;
-  subjObj->relatedCount = 10;
+  subjObj->relatedCount = size;
 }
 
 
--- ntriple2json.h
+++ ntriple2json.h
@@ -1,40 +1,42 @@
 
 struct ntriple {
   char *subject;
-  int subjectSize;
   char *predicate;
-  int predicateSize;
   char *object;
+  int subjectSize;
+  int predicateSize;
   int objectSize;
 };
 
 
 struct subjectObject {
+  char **altLabel;
+  char **narrower;
+  char **broader;
+  char **closeMatch;  
+  char **related;
   char *id;
   char *url;
   char *prefLabel;
-  char **altLabel;
   int altCount;
   int altSize;
-  char **narrower;
   int narrowerCount;
   int narrowerSize;
-  char **broader;
   int broaderCount;
   int broaderSize;
-  char **closeMatch;
   int closeMatchCount;
   int closeMatchSize;
-  char **related;
   int relatedCount;
   int relatedSize;
 };
 
 
 
-void parse(FILE *in_file, FILE *out_file);
+void parse(FILE *in_file, FILE *out_file, int bucket_size);
 struct ntriple get_next_ntriple(FILE *in_file, int *eof);
 char *get_id(char *subject);
+size_t get_hash(char *id, int bucket_size);
+void init_new_entry(struct subjectObject *subjObj, struct ntriple nt, char *id);
 void alloc_new_entry(struct subjectObject *subjObj);
 void add_ntriple_data(struct subjectObject *subjObj, struct ntriple nt);
 int match_tag(char *predicate, char *tag);
